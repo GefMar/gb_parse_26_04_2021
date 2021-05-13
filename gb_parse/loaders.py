2b8a36e80bd05f1803dd189bc7e1ac1fdfe9dcdf
SARomanchuk <SARomanchuk@sberbank.ru>
2021-05-13 20:07:53 +0300

    les 5

--- gb_parse/loaders.py
+++ gb_parse/loaders.py
@@ -30,6 +30,14 @@ def get_characteristics(item: str) -> dict:
     return data
 
 
+def flat_text(items):
+    return "\n".join(items)
+
+
+def hh_user_url(user_id):
+    return urljoin("https://hh.ru/", user_id)
+
+
 class AutoyoulaLoader(ItemLoader):
     default_item_class = dict
     url_out = TakeFirst()
@@ -40,3 +48,14 @@ class AutoyoulaLoader(ItemLoader):
     author_out = TakeFirst()
     description_out = TakeFirst()
     characteristics_in = MapCompose(get_characteristics)
+
+
+class HHLoader(ItemLoader):
+    default_item_class = dict
+    url_out = TakeFirst()
+    title_out = TakeFirst()
+    salary_out = flat_text
+    # description_in = flat_text,
+    # description_out = flat_text,
+    author_in = MapCompose(hh_user_url)
+    author_out = TakeFirst()
